2679defe177b3ee2bc9d4d28ba0223a96333c183
mazbrew <peteryeoh50@gmail.com>
2022-05-24 16:38:58 +0800

    done ?

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.awt.event.KeyEvent;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Robot robo = new Robot();
+        BufferedImage img;
+        int clr,red,green,blue;
+
+        while(true){
+            img = robo.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+            clr = img.getRGB(300, 400);
+
+            red = (clr & 0x00ff0000) >> 16;
+            green = (clr & 0x0000ff00) >> 8;
+            blue = clr & 0x000000ff;
+
+
+            if(red == 172 && blue == 172 && green == 172){
+                robo.keyPress(KeyEvent.VK_SPACE);
+            }
+        }
     }
 }
